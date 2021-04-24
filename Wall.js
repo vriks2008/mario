f9f040140ec0b22f8fff87c9c83fc3351ec7fbab
vriks2008 <81859713+vriks2008@users.noreply.github.com>
2021-04-24 17:26:32 +0530

    Add files via upload

---
+++ Wall.js
@@ -0,0 +1,18 @@
+class Wall {
+    constructor(posX) {
+     
+      this.rx = posX; //setting the x posing where wall will be created  
+      this.ry = height-random([550,250]);   //setting y position where wall will be created 
+      this.spt=createSprite(this.rx, this.ry); //using rx,ry
+      this.spt.shapeColor="green";
+      
+      this.spt.addAnimation("ground",wallAnimation);
+      this.spt.scale=0.10;
+    
+    }
+  
+  
+}
+  
+
+
